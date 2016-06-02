8e2d1a27d788d9b3260a7d550d45faa082ece5a5
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-06-02 12:51:19 +0200

    Added all dependent libraries needed for this project

--- project.clj
+++ project.clj
@@ -1,9 +1,31 @@
+;
+;  (C) Copyright 2016  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (defproject hurvinek "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main ^:skip-aot hurvinek.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+    :description "FIXME: write description"
+    :url "http://example.com/FIXME"
+    :license {:name "Eclipse Public License"
+              :url "http://www.eclipse.org/legal/epl-v10.html"}
+    :dependencies [[org.clojure/clojure "1.6.0"]
+                   [org.clojure/java.jdbc "0.3.5"]
+                   [org.clojure/tools.cli "0.3.1"]
+                   [org.clojure/data.csv  "0.1.2"]
+                   [org.xerial/sqlite-jdbc "3.7.2"]
+                   [clojure-ini "0.0.1"]
+                   [ring/ring-core "1.3.2"]
+                   [ring/ring-jetty-adapter "1.3.2"]
+                   [org.clojure/data.json "0.2.5"]
+                   [hiccup "1.0.4"]]
+    :main ^:skip-aot hurvinek.core
+    :target-path "target/%s"
+    :profiles {:uberjar {:aot :all}})
+
