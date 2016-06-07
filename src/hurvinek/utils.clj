d110c10c9dc3ed5902581410ac7e7c257d73b28c
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-06-07 17:40:56 +0200

    New module 'utils' with various functions that interface with Java classes etc.

---
+++ src/hurvinek/utils.clj
@@ -0,0 +1,46 @@
+;
+;  (C) Copyright 2016  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns hurvinek.utils)
+
+(defn third
+    "Simple utility function - returns third item from a given sequence."
+    [coll]
+    (nth coll 2))
+
+(defn substring
+    "Call method String.substring()."
+    ([^String s from to]
+     (.substring s from to))
+    ([^String s from]
+     (.substring s from)))
+
+(defn startsWith
+    "Call method String.startsWith()."
+    [^String s pattern]
+    (.startsWith s pattern))
+
+(defn endsWith
+    "Call method String.endsWith()."
+    [^String s pattern]
+    (.endsWith s pattern))
+
+(defn contains
+    "Call method String.contains()."
+    [^String s pattern]
+    (.contains s pattern))
+
+(defn replaceAll
+    "Call method String.replaceAll()."
+    [^String s pattern replacement]
+    (.replaceAll s pattern replacement))
+
