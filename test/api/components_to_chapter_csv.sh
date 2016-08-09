b3af11409740c3c52d9eaea97199c168ff2a8a65
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-08-09 12:10:33 +0200

    Added proper (c) header

--- test/api/components_to_chapter_csv.sh
+++ test/api/components_to_chapter_csv.sh
@@ -1,1 +1,15 @@
+#!/bin/sh
+
+#
+#  (C) Copyright 2016  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 curl "localhost:3000/api/components-to-chapter?product-id=RHEL%206&format=csv"
