053ccfb11162d618300aeeb8acf44c577def70f9
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-01 11:41:42 +0100

    Three tests for middleware/inject-configuration function

--- test/hurvinek/middleware_test.clj
+++ test/hurvinek/middleware_test.clj
@@ -11,8 +11,8 @@
 ;
 
 (ns hurvinek.middleware-test
-  (:require [clojure.test :refer :all]
-            [hurvinek.core :refer :all]))
+  (:require [clojure.test        :refer :all]
+            [hurvinek.middleware :refer :all]))
 
 ;
 ; Common functions used by tests.
@@ -31,4 +31,36 @@
     "Check that the hurvinek.middleware/inject-configuration definition exists."
     (testing "if the hurvinek.middleware/inject-configuration definition exists."
         (is (callable? 'hurvinek.middleware/inject-configuration))))
+
+;
+; Tests for function behaviours.
+;
+
+(deftest test-inject-confuguration-1
+    "Check the behaviour of function hurvinek.middleware/inject-configuration."
+    (testing "The function hurvinek.middleware/inject-configuration."
+        (let [function (inject-configuration (fn [x] x) :cfg)]
+            (are [x y] (= x y)
+                {:configuration :cfg} (function nil)
+                {:configuration :cfg} (function {})
+                {:configuration :cfg :foo :bar} (function {:foo :bar})))))
+
+(deftest test-inject-confuguration-2
+    "Check the behaviour of function hurvinek.middleware/inject-configuration."
+    (testing "The function hurvinek.middleware/inject-configuration."
+        (let [function (inject-configuration (fn [x] nil) :cfg)]
+            (are [x y] (= x y)
+                nil (function nil)
+                nil (function {})
+                nil (function {:foo :bar})
+                nil (function {:foo :bar :baz :bar})))))
+
+(deftest test-inject-confuguration-not-NPE
+    "Check the behaviour of function hurvinek.middleware/inject-configuration."
+    (testing "The function hurvinek.middleware/inject-configuration."
+        (let [function (inject-configuration (fn [x] x) nil)]
+            (are [x y] (= x y)
+                {:configuration nil} (function nil)
+                {:configuration nil} (function {})
+                {:configuration nil :foo :bar} (function {:foo :bar})))))
 
