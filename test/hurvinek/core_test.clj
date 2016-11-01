d9149aa0162889a7845ed32717e372e4cb5d060c
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-01 11:43:56 +0100

    Three tests for the function start-server

--- test/hurvinek/core_test.clj
+++ test/hurvinek/core_test.clj
@@ -11,8 +11,10 @@
 ;
 
 (ns hurvinek.core-test
-  (:require [clojure.test :refer :all]
-            [hurvinek.core :refer :all]))
+  (:require [clojure.test       :refer :all]
+            [hurvinek.core      :refer :all]
+            [ring.adapter.jetty :as    jetty]
+            [clojure.tools.cli  :as    cli]))
 
 ;
 ; Common functions used by tests.
@@ -99,4 +101,34 @@
         (is (thrown? AssertionError (get-and-check-port "65536")))
         (is (thrown? AssertionError (get-and-check-port "65537")))
         (is (thrown? AssertionError (get-and-check-port "1000000")))))
+
+(deftest test-start-server-positive-1
+    (testing "hurvinek.core/start-server"
+        ; use mock instead of jetty/run-jetty
+        (with-redefs [jetty/run-jetty (fn [app port] port)]
+            (is (= {:port 1}     (start-server "1")))
+            (is (= {:port 2}     (start-server "2")))
+            (is (= {:port 1000}  (start-server "1000")))
+            (is (= {:port 65534} (start-server "65534")))
+            (is (= {:port 65535} (start-server "65535"))))))
+
+(deftest test-start-server-positive-2
+    (testing "hurvinek.core/start-server"
+        ; use mock instead of jetty/run-jetty
+        (with-redefs [jetty/run-jetty (fn [app port] app)]
+            (is (= app (start-server "1")))
+            (is (= app (start-server "2")))
+            (is (= app (start-server "1000")))
+            (is (= app (start-server "65534")))
+            (is (= app (start-server "65535"))))))
+
+(deftest test-start-server-wrong-port-number
+    (testing "hurvinek.core/start-server"
+        ; use mock instead of jetty/run-jetty
+        (with-redefs [jetty/run-jetty (fn [app port] port)]
+            (is (= {:port -1}      (start-server "-1")))
+            (is (= {:port 0}       (start-server "0")))
+            (is (= {:port 65536}   (start-server "65536")))
+            (is (= {:port 65537}   (start-server "65537")))
+            (is (= {:port 1000000} (start-server "1000000"))))))
 
