a34f21b9b0e46c17b6b004261d5f8564d7e934ae
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-18 12:15:24 +0100

    Bump up clojure version to 1.7.0

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
     :url "http://example.com/FIXME"
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.6.0"]
+    :dependencies [[org.clojure/clojure "1.7.0"]
                    [org.clojure/java.jdbc "0.3.5"]
                    [org.clojure/tools.cli "0.3.1"]
                    [org.clojure/data.csv  "0.1.2"]
